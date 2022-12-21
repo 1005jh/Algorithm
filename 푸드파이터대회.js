205a99ebafa79e8997b45a2f4f669da171890d75
jeonghyeon <kjhhy1005@gmail.com>
2022-12-21 13:40:22 +0900

    fix : 다른버전 추가

--- 푸드파이터대회.js
+++ 푸드파이터대회.js
@@ -1,12 +1,19 @@
-function solution(food) {
-  const answer = food.reverse().reduce((a, c, i) => {
-    const foodNum = (food.length - i - 1).toString();
-    // console.log(foodNum, a, c, i);
-    const repeatFood = foodNum.repeat(c / 2);
-    return repeatFood + a + repeatFood;
-  }, "0");
-  return answer;
-}
+//*정현 풀이
+// function solution(food) {
+//   const answer = food.reverse().reduce((a, c, i) => {
+//     const foodNum = (food.length - i - 1).toString();
+//     // console.log(foodNum, a, c, i);
+//     const repeatFood = foodNum.repeat(c / 2);
+//     return repeatFood + a + repeatFood;
+//   }, "0");
+//   return answer;
+// }
+//*세준 풀이
+// function solution(food) {
+//   const newFood = food.map((item, i) => parseInt(item / 2)).slice(1);
+//   const result = newFood.reduce((a, b, i) => a + (i + 1 + "").repeat(b), "");
+//   return result + "0" + [...result].reverse().join("");
+// }
 
 let food = [
   1,
