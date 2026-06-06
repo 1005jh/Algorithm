89d5266423666cb51710776fb36cf4712b13c561
jeonghyeon <kjhhy1005@gmail.com>
2026-06-06 23:43:53 +0900

    정수삼각py

---
+++ 정수삼각형.py
@@ -0,0 +1,8 @@
+def solution(triangle):
+    height = len(triangle)
+    
+    for i in range(height - 2, -1, -1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+            
+    return triangle[0][0]
